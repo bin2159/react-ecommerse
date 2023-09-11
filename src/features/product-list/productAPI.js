2e8b780e1d41af4b6e143745f83575f8a714de31
bin2159 <bipinthms007@gmail.com>
2023-09-11 17:48:19 +0530

    api using json-server , product filtering added

--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -1,9 +1,33 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise(async (resolve) =>{
-    const response=await fetch('http://localhost:8080')
-    const data=await response.json()
-    resolve({data})  
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hardcord server URL  here
+    const response = await fetch('http://localhost:8080/products')
+    const data = await response.json()
+    resolve({ data })
+  })
+}
+
+export function fetchAllProductsByFilter({filter,sort}) {
+  //filter={"category":["smartphone","laptops"]}
+  //sort={_sort:"price",_order="desc"}
+  let queryString = ''
+  for (let key in filter) {
+    const categoryValues = filter[key]
+    if (categoryValues.length > 0) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
   }
-  );
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`
+  }
+  return new Promise(async (resolve) => {
+    //TODO: we will not hardcord server URL  here
+    const response = await fetch(
+      'http://localhost:8080/products?' + queryString
+    )
+    const data = await response.json()
+    resolve({ data })
+  })
 }
